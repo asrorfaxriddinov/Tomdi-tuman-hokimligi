fc1c6e20fe519beb6040e4b8e5d6834a194c995a
asrorfaxriddinov <148222396+asrorfaxriddinov@users.noreply.github.com>
2024-02-25 13:46:27 +0500

    Tomdi Tumani
    Tomdi tuman hokimligi sayti Yangich

---
+++ TomdiHokimligi/js/nev_style.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded',function(e){
+//     let div = document.getElementById('dropdown');
+//     let a1 = document.getElementById('a1');
+  
+//     div.style.cssText=`
+    
+//     `
+// })
---
+++ TomdiHokimligi/js/yangi.js
@@ -0,0 +1,247 @@
+document.addEventListener('DOMContentLoaded',function(e){
+    let terget = document.getElementById('about-liner')
+    let kun = document.getElementById('kun');
+    let kun1 = document.getElementById('kun1');
+    let tun = document.getElementById('tun');
+    let tun1 = document.getElementById('tun1');
+    let outer1 = document.querySelector('.outer-box');
+    let outer2 = document.querySelector('.outer-container');
+    let outer3 = document.querySelector('.container-fluid');
+    let outer4 = document.querySelector('#outer');
+    let rang = document.getElementById('rang'); 
+    let language = document.querySelector('.language');
+    let language1 = document.querySelector('.language1');
+    let language2 = document.querySelector('.language2');
+    let language3 = document.querySelector('.language3');
+    let language4 = document.querySelector('.language4');
+    let border = document.getElementById('border');
+    let border1 = document.getElementById('border1');
+    let rasm = document.getElementById('rasm');
+    let rasm1 = document.getElementById('rasm1');
+    let rasm2 = document.getElementById('rasm2');
+    let xoshia = document.getElementById('xoshia');
+    let xoshia1 = document.getElementById('xoshia1');
+    let fact = document.getElementById('fact');
+    let a1 = document.getElementById('a1');
+    let a2 = document.getElementById('a2');
+    let a3 = document.getElementById('a3');
+    let a4 = document.getElementById('a4');
+    let a5 = document.getElementById('a5');
+    let a6 = document.getElementById('a6');
+    let goo = document.getElementById('goo');
+    let fa = document.getElementById('fa');
+    let slogan = document.getElementById('slogan');
+    let about = document.getElementById('about');
+    let content = document.querySelector('.content');
+    let divs = document.getElementById('divs');
+    let event = document.getElementById('event');
+   let area = document.getElementById('area');
+   let footer = document.getElementById('footer');
+    let bottom = document.getElementById('bottom'); 
+    let border2 = document.querySelector('.border-left');
+    let colors = document.getElementById('colors');
+    let colors1 = document.getElementById('colors');
+    let FColor = document.querySelectorAll('.FColor');
+    kun.addEventListener('click',function(e){
+       about.style.background = '#0e1833';
+       bottom.style.background = '#0e1833';
+       footer.style.background = '#0e1833';
+       area.style.background = '#0e1833';
+       fact.style.background = '#0e1833';
+       event.style.background = '#0e1833';
+       divs.style.background = '#fff';
+       slogan.style.background = '#0e1833';
+       fa.style.border = '2px solid black';
+       border2.style.borderLeft = '2px solid black';
+       content.style.color = '#fff';
+       colors.style.color = '#fff';
+       for (let i = 0; i < FColor.length; i++) {
+         FColor[i].style.color = "#fff";
+      }
+       colors1.style.color = '#fff';
+       a1.style.color = '#fff';
+       a2.style.color = '#fff';
+       a3.style.color = '#fff';
+       a4.style.color = '#fff';
+       a5.style.color = '#fff';
+       a6.style.color = '#fff';
+       kun.style.display = 'none' ;
+       tun.style.display = 'block' ;
+       xoshia.style.border = "3px solid #fff";
+       xoshia1.style.border = "3px solid #fff";
+       outer1.style.background = '#0e1833';
+       outer2.style.background = '#0e1833';
+       outer3.style.background = '#0e1833';
+       outer4.style.background = '#0e1833';
+       terget.style.background = '#0e1833';
+       rang.setAttribute("style","color:#fff");
+       language,language2 ,language1.setAttribute("style","color:#fff");
+       language,language2 ,language2.setAttribute("style","color:#fff");
+       language1,language2,language.setAttribute("style","color:#fff");
+       language3.setAttribute("style","color:#fff");
+       language4.setAttribute("style","color:#fff");
+       border.setAttribute("style","border:2px solid #fff;border-radius: 20px;width: 30px;padding: 3px;");
+       border1.setAttribute("style","border:2px solid #fff;border-radius: 20px;width: 30px;padding-left: 7px;");
+       rasm.src = "images/logo-uz-white.png";
+       rasm.style.width = "350px";
+       rasm.style.height = "90px";
+       rasm1.src = "images/logo-uz-white.png";
+       rasm1.style.width = "350px";
+       rasm1.style.height = "90px";
+       rasm2.src = "images/logo-uz-white.png";
+       rasm2.style.width = "350px";
+       rasm2.style.height = "90px";
+    });
+    kun1.addEventListener('click',function(e){
+      for (let i = 0; i < FColor.length; i++) {
+         FColor[i].style.color = "#fff";
+      }
+      about.style.background = '#0e1833';
+      bottom.style.background = '#0e1833';
+      footer.style.background = '#0e1833';
+      area.style.background = '#0e1833';
+      fact.style.background = '#0e1833';
+      event.style.background = '#0e1833';
+      divs.style.background = '#fff';
+      slogan.style.background = '#0e1833';
+      fa.style.border = '2px solid black';
+      border2.style.borderLeft = '2px solid black';
+      content.style.color = '#fff';
+      colors.style.color = '#fff';
+      colors1.style.color = '#fff';
+      a1.style.color = '#fff';
+   
+      a2.style.color = '#fff';
+      a3.style.color = '#fff';
+      a4.style.color = '#fff';
+      a5.style.color = '#fff';
+      a6.style.color = '#fff';
+      kun1.style.display = 'none' ;
+      tun1.style.display = 'block' ;
+      xoshia.style.border = "3px solid #fff";
+      xoshia1.style.border = "3px solid #fff";
+      outer1.style.background = '#0e1833';
+      outer2.style.background = '#0e1833';
+      outer3.style.background = '#0e1833';
+      outer4.style.background = '#0e1833';
+      terget.style.background = '#0e1833';
+      rang.setAttribute("style","color:#fff");
+      language,language2 ,language1.setAttribute("style","color:#fff");
+      language,language2 ,language2.setAttribute("style","color:#fff");
+      language1,language2,language.setAttribute("style","color:#fff");
+      language3.setAttribute("style","color:#fff");
+      language4.setAttribute("style","color:#fff");
+      border.setAttribute("style","border:2px solid #fff;border-radius: 20px;width: 30px;padding: 3px;");
+      border1.setAttribute("style","border:2px solid #fff;border-radius: 20px;width: 30px;padding-left: 7px;");
+      rasm.src = "images/logo-uz-white.png";
+      rasm.style.width = "350px";
+      rasm.style.height = "90px";
+      rasm1.src = "images/logo-uz-white.png";
+      rasm1.style.width = "350px";
+      rasm1.style.height = "90px";
+      rasm2.src = "images/logo-uz-white.png";
+      rasm2.style.width = "350px";
+      rasm2.style.height = "90px";
+   });
+    tun.addEventListener('click',function(e){
+      fa.setAttribute('style','border:2px solid black;border-radius:40%;padding:10px 10px 10px 10px;')
+       goo.style.border = '2px solid black';
+       bottom.style.background = '#a0e6ff';
+       footer.style.background = '#a0e6ff';
+       divs.style.background = '#000'
+       area.style.background = '#a0e6ff'
+       about.style.background = '#a0e6ff'
+       fact.style.background = '#a0e6ff';
+       event.style.background = '#a0e6ff';
+       slogan.style.background = '#a0e6ff'
+       content.style.color = '#0e1833';
+       colors.style.color = '#0e1833';
+       colors1.style.color = '#0e1833';
+       a1.style.color = '#0e1833';
+       for (let i = 0; i < FColor.length; i++) {
+         FColor[i].style.color = "#0e1833";
+      }
+       a2.style.color = '#0e1833';
+       a3.style.color = '#0e1833';
+       a4.style.color = '#0e1833';
+       a5.style.color = '#0e1833';
+       a6.style.color = '#0e1833';
+       tun.style.display = 'none' ;
+       kun.style.display = 'block' ;
+       xoshia.style.border = "3px solid #0e1833";
+       xoshia1.style.border = "3px solid #0e1833";
+       outer1.style.background = '#a0e6ff';
+       outer2.style.background = '#a0e6ff';
+       outer3.style.background = '#a0e6ff';
+       outer4.style.background = '#a0e6ff';
+       terget.style.background = '#a0e6ff';
+       rang.setAttribute("style","color:black");
+       language,language2,language1.setAttribute("style","color:black");
+       language,language2,language2.setAttribute("style","color:black");
+       language1,language2,language.setAttribute("style","color:black");
+       language3.setAttribute("style","color:black");
+       language4.setAttribute("style","color:black");
+       border.setAttribute("style","border:2px solid black;border-radius: 20px;width: 30px;padding: 3px;");
+       border1.setAttribute("style","border:2px solid black;border-radius: 20px;width: 30px;padding-left: 7px;");
+      rasm.src = "images/rasm.png";
+      rasm.style.width = "350px";
+      rasm.style.height = "90px";
+      rasm1.src = "images/rasm.png";
+      rasm1.style.width = "350px";
+      rasm1.style.height = "90px";
+      rasm2.src = "images/rasm.png";
+      rasm2.style.width = "350px";
+      rasm2.style.height = "90px";
+     });
+     tun1.addEventListener('click',function(e){
+      fa.setAttribute('style','border:2px solid black;border-radius:40%;padding:10px 10px 10px 10px;');
+      for (let i = 0; i < FColor.length; i++) {
+         FColor[i].style.color = "#0e1833";
+      }
+       goo.style.border = '2px solid black';
+       bottom.style.background = '#a0e6ff';
+       footer.style.background = '#a0e6ff';
+       divs.style.background = '#000'
+       area.style.background = '#a0e6ff'
+       about.style.background = '#a0e6ff'
+       fact.style.background = '#a0e6ff';
+       event.style.background = '#a0e6ff';
+       slogan.style.background = '#a0e6ff'
+       content.style.color = '#0e1833';
+       colors.style.color = '#0e1833';
+       colors1.style.color = '#0e1833';
+       a1.style.color = '#0e1833';
+       a2.style.color = '#0e1833';
+       a3.style.color = '#0e1833';
+       a4.style.color = '#0e1833';
+       
+       a5.style.color = '#0e1833';
+       a6.style.color = '#0e1833';
+       tun1.style.display = 'none' ;
+       kun1.style.display = 'block' ;
+       xoshia.style.border = "3px solid #0e1833";
+       xoshia1.style.border = "3px solid #0e1833"
+       outer1.style.background = '#a0e6ff';
+       outer2.style.background = '#a0e6ff';
+       outer3.style.background = '#a0e6ff';
+       outer4.style.background = '#a0e6ff';
+       terget.style.background = '#a0e6ff';
+       rang.setAttribute("style","color:black");
+       language,language2,language1.setAttribute("style","color:black");
+       language,language2,language2.setAttribute("style","color:black");
+       language1,language2,language.setAttribute("style","color:black");
+       language3.setAttribute("style","color:black");
+       language4.setAttribute("style","color:black");
+       border.setAttribute("style","border:2px solid black;border-radius: 20px;width: 30px;padding: 3px;");
+       border1.setAttribute("style","border:2px solid black;border-radius: 20px;width: 30px;padding-left: 7px;");
+      rasm.src = "images/rasm.png";
+      rasm.style.width = "350px";
+      rasm.style.height = "90px";
+      rasm1.src = "images/rasm.png";
+      rasm1.style.width = "350px";
+      rasm1.style.height = "90px";
+      rasm2.src = "images/rasm.png";
+      rasm2.style.width = "350px";
+      rasm.style.height = "90px";
+     });
+});
